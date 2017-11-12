81c969294744f4aee1bf5db81dcd1437a6c2d443
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ function lowerCaseDrivers(array) {
 
 
 function nameToAttributes(names){
+  names.map(function(name){
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return {firstname: fName, LastName: lName}
+
+    
+  })
 
 
 
