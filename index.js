9c40c2b3001cdea82a6d4e658d726a4bfbedddc8
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function nameToAttributes(names){
   names.map(function(name){
     let fName = name.split(" ")[0]
     let lName = name.split(" ")[1]
-    return {firstname: fName, LastName: lName}
+    return {firstName: fName, lastName: lName}
 
-    
+
   })
 
 
