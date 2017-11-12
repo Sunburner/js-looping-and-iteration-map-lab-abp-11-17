aa5e4bca660a9bf060af836c9dbb0bef4bd89173
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+lunction lowerCaseDrivers(array) {
+  return array.map(function(e) {
+    return e.toLowerCase()
+    
+  })
+  
+}
+
+
+function nameToAttributes(names){
+  
+
+
+  
+}
